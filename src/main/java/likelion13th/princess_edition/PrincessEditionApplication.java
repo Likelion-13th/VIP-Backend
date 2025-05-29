c0d07eec671b512a3b21ddcb4ec66df92d05f80b
silver-kg <kge8606@kau.kr>
2025-05-29 19:57:34 +0900

    princess-start

---
+++ src/main/java/likelion13th/princess_edition/PrincessEditionApplication.java
@@ -0,0 +1,13 @@
+package likelion13th.princess_edition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincessEditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincessEditionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion13th/princess_edition/PrincessEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.princess_edition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincessEditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
