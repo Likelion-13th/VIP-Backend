7cc746ad67afd579bbdd148e67f259aa2b05c375
silver-kg <kge8606@kau.kr>
2025-11-06 19:47:52 +0900

    25.08.05 과제/미완

--- src/main/java/likelion13th/princess_edition/login/auth/utils/OAuth2SuccessHandler.java
+++ src/main/java/likelion13th/princess_edition/login/auth/utils/OAuth2SuccessHandler.java
@@ -31,7 +31,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
             "https://vip-likelion.netlify.app",
             "http://localhost:3000"
     );
-    private static final String DEFAULT_FRONT_ORIGIN = "https://jimalshop.netlify.app";
+    private static final String DEFAULT_FRONT_ORIGIN = "https://vip-likelion.netlify.app";
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
